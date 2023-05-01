10a0fa5fb192d761c7b9b330ac0797330668f1e9
Suman Balayar <076bei042.suman@pcampus.edu.np>
2023-05-01 15:12:47 +0545

    Initial Backend code

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const express = require("express");
-
+const express = require("express"); //To initialize the instance for express
+const cors = require("cors"); //Used to resolve the fetch api problem from frontend part
+require('./db/config');
 const app = express();
+const User = require('./db/User');
 
-app.get("/",(req,res)=>{
-    res.send("App is Working")
+app.use(express.json());//MiddleWare is use for handling the data from frontend part
+app.use(cors());
+
+app.post("/register",(req,res)=>{
+    const myData = new User(req.body);
+    myData.save();
+    res.send(req.body);
 });
 
-app.listen(5000)
+
+app.listen(5000);
