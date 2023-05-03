938e68bde243f6a73f23b25189d5766d9a057ddf
Suman Balayar <076bei042.suman@pcampus.edu.np>
2023-05-03 14:59:58 +0545

    Initial Code

--- db/config.js
+++ db/config.js
@@ -1,3 +1,8 @@
 const mongoose=require("mongoose");
-mongoose.connect('mongodb://localhost:27017/e-commerce');//Local Host for Mongodb Connection
+mongoose.connect('mongodb://localhost:27017/e-commerce')
+.then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB', err);
+});//Local Host for Mongodb Connection
 
--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require("express"); //To initialize the instance for express
 const cors = require("cors"); //Used to resolve the fetch api problem from frontend part
-require('./db/config');
+const bcrypt = require("bcryptjs");
+require("./db/config");
 const app = express();
-const User = require('./db/User');
+const User = require("./db/User");
 
-app.use(express.json());//MiddleWare is use for handling the data from frontend part
+app.use(express.json()); //MiddleWare is use for handling the data from frontend part
 app.use(cors());
 
-app.post("/register",(req,res)=>{
-    const myData = new User(req.body);
-    myData.save();
-    res.send(req.body);
+app.post("/register", async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const myData = await new User({
+    name:req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  myData.save();
+  res.send(req.body);
 });
 
+app.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.send("Invalid email");
+  }
+
+  const passwordMatches = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (!passwordMatches) {
+    return res.send("Invalid password");
+  }
+  res.send(user);
+});
 
 app.listen(5000);
